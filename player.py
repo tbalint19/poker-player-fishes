b992ecb126dd75f43d7826459cf944d9e8258347
tbalint19 <toth910719balint@gmail.com>
2016-12-03 10:20:34 +0100

    3st

--- player.py
+++ player.py
@@ -11,7 +11,20 @@ class Player:
         if fishes["hole_cards"][0]["rank"] == fishes["hole_cards"][1]["rank"]:
             return_value = 10000
         else:
-            return_value = 0
+            first_good = False
+            second_good = False
+            try:
+                first = int(fishes["hole_cards"][0]["rank"])
+            except:
+                first_good = True
+            try:
+                second = int(fishes["hole_cards"][0]["rank"])
+            except:
+                second_good = True
+            if first_good and second_good:
+                return_value = 10000
+            else:
+                return_value = 0
 
         return return_value
 
