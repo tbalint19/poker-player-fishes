26ca4bc5aeedc91c15a20506d7a983abdf9386c7
tbalint19 <toth910719balint@gmail.com>
2016-12-03 10:49:43 +0100

    7th

--- player.py
+++ player.py
@@ -4,37 +4,41 @@ class Player:
 
     def betRequest(self, game_state):
 
-        # fishes = self.get_my_hand()
-        # pairs_in_hand = self.check_hand_for_pairs(fishes["hole_cards"])
-        #
-        # if fishes["hole_cards"][0]["rank"] == fishes["hole_cards"][1]["rank"]:
-        #     return_value = 10000
-        # else:
-        #     first_good = False
-        #     second_good = False
-        #     try:
-        #         first = int(fishes["hole_cards"][0]["rank"])
-        #     except:
-        #         first_good = True
-        #     try:
-        #         second = int(fishes["hole_cards"][0]["rank"])
-        #     except:
-        #         second_good = True
-        #     if first_good and second_good:
-        #         return_value = 10000
-        #     else:
-        #         return_value = 0
+        try:
 
-        return 0
+            fishes = self.get_my_hand()
+            pairs_in_hand = self.check_hand_for_pairs(fishes["hole_cards"])
+            both_figures = self.check_hand_for_figures(fishes["hole_cards"])
+
+            if pairs_in_hand or both_figures:
+                return 10000
+            else:
+                return 0
+
+        except:
+            return 10000
 
     def showdown(self, game_state):
         pass
 
-    # def get_my_hand(self, game_state):
-    #     all_data = game_state
-    #     for player in all_data["players"]:
-    #         if player["name"] == "Fishes":
-    #             return player
-    #
-    # def check_hand_for_pairs(self, cards):
-    #     return cards[0]["rank"] == cards[0]["rank"]
+    def get_my_hand(self, game_state):
+        all_data = game_state
+        for player in all_data["players"]:
+            if player["name"] == "Fishes":
+                return player
+
+    def check_hand_for_pairs(self, cards):
+        return cards[0]["rank"] == cards[0]["rank"]
+
+    def check_hand_for_figures(self, cards):
+        first_good = False
+        second_good = False
+        try:
+            first = int(fishes["hole_cards"][0]["rank"])
+        except:
+            first_good = True
+        try:
+            second = int(fishes["hole_cards"][0]["rank"])
+        except:
+            second_good = True
+        return first_good and second_good
