8a1352b231a883bfdb0feaf3748028ee3f0b57e4
tbalint19 <toth910719balint@gmail.com>
2016-12-03 10:13:57 +0100

    1st

--- player.py
+++ player.py
@@ -3,7 +3,17 @@ class Player:
     VERSION = "bela"
 
     def betRequest(self, game_state):
-        return 10000
+        all_data = game_state
+        for player in all_data["players"]:
+            if player["name"] == "Fishes":
+                fishes = player
+
+        if fishes["hole_cards"][0]["rank"] == fishes["hole_cards"][1]["rank"]:
+            return_value = 10000
+        else:
+            return_value = 0
+
+        return return_value
 
     def showdown(self, game_state):
         pass
