c9a4e970001324393b866eb8d802d555d3721744
tbalint19 <toth910719balint@gmail.com>
2016-12-03 11:22:30 +0100

    11th

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player:
         try:
 
             fishes = self.get_my_hand()
-            pairs_in_hand = self.check_hand_for_pairs(fishes["hole_cards"])
+            # pairs_in_hand = self.check_hand_for_pairs(fishes["hole_cards"])
             both_figures = self.check_hand_for_figures(fishes["hole_cards"])
 
             if pairs_in_hand or both_figures:
@@ -38,7 +38,7 @@ class Player:
         except:
             first_good = True
         try:
-            second = int(cards[0]["rank"])
+            second = int(cards[1]["rank"])
         except:
             second_good = True
         return first_good and second_good
